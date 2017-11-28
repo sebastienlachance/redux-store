364ae649abf0e81dda0d9df9f0e754e8468c5d44
Sébastien Lachance <slachance@garaga.com>
2017-11-28 15:18:08 -0500

    Basic Store

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import * as fromStore from './store';
+
 import { renderTodos } from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
@@ -5,12 +7,20 @@ const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
 
+const store = new fromStore.Store({}, {
+    todos: [{ label: 'Eat Pizza', complete: false }]
+});
+
+console.log(store.value);
+
 button.addEventListener(
   'click',
   () => {
     if (!input.value.trim()) return;
 
     const payload = { label: input.value, complete: false };
+
+    // store.dispatch({ action: '', payload: payload});
 
     console.log(payload);
 
---
+++ src/store/store.ts
@@ -0,0 +1,19 @@
+export class Store {
+    private subscribers: Function[];
+    private reducers: { [key: string]: Function };
+    private state: { [key: string]: any };
+
+    constructor(reducers = {}, initialState = {}) {
+        this.state = initialState;
+    }
+
+    get value() {
+        return this.state;
+    }
+
+    dispatch(action) {
+        this.state = {
+            ...this.state
+        };
+    }
+}
